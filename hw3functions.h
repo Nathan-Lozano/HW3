8e42b6b2dab8c794ec0e43809be498f60bcb577f
Nathan-Lozano <63679649+Nathan-Lozano@users.noreply.github.com>
2020-04-18 19:40:06 -0500

    Update hw3functions.h

--- hw3functions.h
+++ hw3functions.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <unistd.h>
 using namespace std;
 
 void showMenu(){
@@ -17,6 +18,29 @@ void getRational(int *num, int *den){
     getline(stream1, denstring, '/');
     *num = stoi(numstring);
     *den = stoi(denstring);
+    if(*den == 0)
+        system("EXIT");
+}
+
+void reduce(int *num, int *den){
+    int x = *num % *den;
+    int a = *num, b = *den;
+    while(x != 0){
+        a = b;
+        b = x;
+        x = a % b;
+        //cout << x;
+    }
+    *num /= b;
+    *den /= b;
+}
+
+void addRational(int num1, int den1, int num2, int den2, int *inum, int *iden){
+    reduce(&num1, &den1);
+    reduce(&num2, &den2);
+    *inum = (num1 * den2) + (num2 * den1); 
+    *iden = den1 * den2;
+    cout << *inum << "/" << *iden;
 }
 
 void add(){
@@ -26,7 +50,7 @@ void add(){
     cout << "Addition of rational numbers" << endl;
     getRational(&num1, &den1);
     getRational(&num2, &den2);
-    cout << num1 << " " << den1 << endl << num2 << " " << den2;
+    addRational(num1, den1, num2, den2, &inum, &iden);
     //cout << "Do you want to do more additions?(Y/N) "
     //cin >> cont;
     //}
@@ -40,3 +64,6 @@ void subtract(){
     //cin >> cont;
     //}
 }
+
+
+
